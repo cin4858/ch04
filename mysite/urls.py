a73a016249d7da0ac63b5cf30fd5b11c8d102f59
cin4858 <cin4858@naver.com>
2018-09-22 05:35:38 +0900

    bookmark_first

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,18 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
+# from bookmark.views import BookmarkLV, BookmarkDV
 from bookmark.views import BookmarkLV, BookmarkDV
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', include('bookmark.urls')),
     # url(regex, view, kwargs=None, name=None, prefix='')
     #  여기서 regex와 view는 필수 인자)
     #  북마크 앱을 위한 클래스 기반 뷰
     #  /bookmark/ 요청을 처리할 뷰 클래스를 BookmarkLV로 지정하고, URL 패턴 이름 지정
     url(r'^bookmark/$', BookmarkLV.as_view(), name='index'),
     #  /bookmark/숫자/ 요청을 처리할 뷰 클래스를 BookmarkDV로 지정하고, URL 패턴 이름 지정
-    url(r'^bookmark/(?P<pk>\d+)/$', BookmarkDV.as_view(), name='detail'),
+    url(r'^bookmark/(?P<pk>\d+)/$', BookmarkDV.as_view(), name='detail')
 ]
