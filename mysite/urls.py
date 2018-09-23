f4eb6732319ec254a097344696a3e0721ed97be5
cin4858 <cin4858@naver.com>
2018-09-23 17:42:53 +0900

    Changed ...

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 # from bookmark.views import BookmarkLV, BookmarkDV
-from bookmark.views import BookmarkLV, BookmarkDV
+ # from bookmark.views import BookmarkLV, BookmarkDV
 
 
 urlpatterns = [
@@ -27,7 +27,4 @@ urlpatterns = [
     #  여기서 regex와 view는 필수 인자)
     #  북마크 앱을 위한 클래스 기반 뷰
     #  /bookmark/ 요청을 처리할 뷰 클래스를 BookmarkLV로 지정하고, URL 패턴 이름 지정
-    url(r'^bookmark/$', BookmarkLV.as_view(), name='index'),
-    #  /bookmark/숫자/ 요청을 처리할 뷰 클래스를 BookmarkDV로 지정하고, URL 패턴 이름 지정
-    url(r'^bookmark/(?P<pk>\d+)/$', BookmarkDV.as_view(), name='detail')
 ]
